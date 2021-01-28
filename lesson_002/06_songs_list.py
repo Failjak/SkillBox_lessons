138f8a38b6663ac964186654a22a7c7d74e9f70b
Nikita <nikia.gushtyn200370@gmail.com>
2021-01-29 00:01:23 +0300

    The 7'th task are ready.

--- lesson_002/06_songs_list.py
+++ lesson_002/06_songs_list.py
@@ -19,8 +19,19 @@ violator_songs_list = [
 #   Три песни звучат ХХХ минут
 # Обратите внимание, что делать много вычислений внутри print() - плохой стиль.
 # Лучше заранее вычислить необходимое, а затем в print(xxx, yyy, zzz)
+i = 0
+while i < len(violator_songs_list):
+    if 'Halo' in violator_songs_list[i]:
+        ind_halo = i
+    if 'Enjoy the Silence' in violator_songs_list[i]:
+        ind_ehjoy = i
+    if 'Clean' in violator_songs_list[i]:
+        ind_clean = i
+    i = i + 1
 
-# TODO здесь ваш код
+duration = violator_songs_list[ind_halo][1] + violator_songs_list[ind_ehjoy][1] + violator_songs_list[ind_clean][1]
+print('Три песни звучат', round(duration, 2), 'минут')
+
 
 # Есть словарь песен группы Depeche Mode
 violator_songs_dict = {
@@ -38,4 +49,6 @@ violator_songs_dict = {
 # распечатайте общее время звучания трех песен: 'Sweetest Perfection', 'Policy of Truth' и 'Blue Dress'
 #   А другие три песни звучат ХХХ минут
 
-# TODO здесь ваш код
+duration_2 = violator_songs_dict['Sweetest Perfection'] + violator_songs_dict['Policy of Truth'] + violator_songs_dict['Blue Dress']
+
+print("А другие три песни звучат", round(duration_2), 'минут')

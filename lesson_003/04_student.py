18ebe323245686f39da4692784e0412234ddc4f4
Nikita <nikia.gushtyn200370@gmail.com>
2021-01-29 23:10:28 +0300

    4'd task is ready.

--- lesson_003/04_student.py
+++ lesson_003/04_student.py
@@ -11,4 +11,14 @@
 
 educational_grant, expenses = 10000, 12000
 
-# TODO здесь ваш код
+educational_grant_year = 10 * educational_grant + .0
+expenses_year = new_expenses = expenses + .0
+i = 1
+while i < 10:
+    new_expenses *= 1.03
+    expenses_year += new_expenses
+    i += 1
+print(round(expenses_year, 2), 'руб.')
+
+parents_money = expenses_year - educational_grant_year
+print('Стеденту надо попросить', round(parents_money, 2))

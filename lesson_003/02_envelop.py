f2858bbb230732b9934a207126ac4c24a9004bdd
Nikita <nikia.gushtyn200370@gmail.com>
2021-01-29 22:07:53 +0300

    Second task is ready.

--- lesson_003/02_envelop.py
+++ lesson_003/02_envelop.py
@@ -20,7 +20,13 @@ paper_x, paper_y = 8, 9
 # paper_x, paper_y = 9, 11
 # (просто раскоментировать нужную строку и проверить свой код)
 
-# TODO здесь ваш код
+print('First task:')
+if envelop_x >= paper_x and envelop_y >= paper_y:
+    print("Yes")
+elif envelop_x >= paper_y and envelop_y >= paper_x:
+    print('Yes')
+else:
+    print('No')
 
 # Усложненное задание, решать по желанию.
 # Заданы размеры hole_x, hole_y прямоугольного отверстия и размеры brick_х, brick_у, brick_z кирпича (все размеры
@@ -28,8 +34,8 @@ paper_x, paper_y = 8, 9
 #
 # Определить, пройдет ли кирпич через отверстие (грани кирпича параллельны сторонам отверстия)
 
-# hole_x, hole_y = 8, 9
-# brick_x, brick_y, brick_z = 11, 10, 2
+hole_x, hole_y = 8, 9
+brick_x, brick_y, brick_z = 11, 10, 2
 # brick_x, brick_y, brick_z = 11, 2, 10
 # brick_x, brick_y, brick_z = 10, 11, 2
 # brick_x, brick_y, brick_z = 10, 2, 11
@@ -48,5 +54,12 @@ paper_x, paper_y = 8, 9
 # brick_x, brick_y, brick_z = 3, 6, 11
 # brick_x, brick_y, brick_z = 3, 11, 6
 # (просто раскоментировать нужную строку и проверить свой код)
-
-# TODO здесь ваш код
+print('\nSecond task:')
+if hole_x >= brick_x and (hole_y >= brick_z or hole_y >= brick_y):
+    print('Yes')
+elif hole_x >= brick_y and (hole_y >= brick_z or hole_y >= brick_x):
+    print('Yes')
+elif hole_x >= brick_z and (hole_y >= brick_x or hole_y >= brick_y):
+    print('Yes')
+else:
+    print('NO!!!')

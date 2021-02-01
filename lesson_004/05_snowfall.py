291ea57eccbec4d1782a1ea3377c2d70eaffc16d
Nikita <nikia.gushtyn200370@gmail.com>
2021-02-02 01:31:05 +0300

    Совершенно не готов, сделаю утром

--- lesson_004/05_snowfall.py
+++ lesson_004/05_snowfall.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import simple_draw as sd
+import random
 
 
 # На основе кода из практической части реализовать снегопад:
@@ -8,7 +9,6 @@ import simple_draw as sd
 # - нарисовать падение этих N снежинок
 # - создать список рандомных длинн лучей снежинок (от 10 до 100) и пусть все снежинки будут разные
 
-N = 20
 
 # Пригодятся функции
 # sd.get_point()
@@ -16,18 +16,53 @@ N = 20
 # sd.sleep()
 # sd.random_number()
 # sd.user_want_exit()
+def snowfall(n, coord, wind, speed):
+    lenght = 20
+    while True:
+        for i in range(n):
+            x0, y0 = coord[i][0], coord[i][1]
+            point = sd.get_point(x0, y0)
+            sd.snowflake(center=point, length=lenght)
+            if coord[i][1] <= lenght and speed != 0:
+                speed[i] = 0
+                wind[i] = 0
+                # coord.pop([i][0])
+                # coord.pop([i][1])
+            if coord[i][0] >= 580 and wind[i] > 0:
+                wind[i] = 0
+                # coord.pop([i][0])
+                # coord.pop([i][1])
+            if coord[i][0] <= 20 and wind[i] < 0:
+                wind[i] = 0
+                # coord.pop([i][0])
+                # coord.pop([i][1])
+            sd.sleep(.1)
+        for i in range(n):
+            x0, y0 = coord[i][0], coord[i][1]
+            point = sd.get_point(x0, y0)
+            sd.snowflake(center=point, length=lenght, color=sd.background_color)
+        coord[i][0] -= wind[i]
+        coord[i][1] -= speed[i]
+        if sd.user_want_exit():
+            break
+    sd.pause()
 
-# TODO здесь ваш код
-while True:
-    sd.clear_screen()
-    pass
-    pass
-    pass
-    sd.sleep(0.1)
-    if sd.user_want_exit():
-        break
 
-sd.pause()
+def main():
+    speed = []
+    wind = []
+    snowflake_coord = []
+    # quan = int(input('Enter the num of snowflakes:'))
+    for i in range(3):
+        speed.append(random.randint(10, 50))
+        wind.append(random.randint(-20, 20))
+        x = random.randint(50, 550)
+        y = random.randint(400, 550)
+        snowflake_coord.append([x, y])
+    snowfall(3, snowflake_coord, wind, speed)
+
+
+main()
 
 # подсказка! для ускорения отрисовки можно
 #  - убрать clear_screen()

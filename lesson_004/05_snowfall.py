593b3baec3ac0657d3af0d4b58100922fcbb5680
Nikita <nikia.gushtyn200370@gmail.com>
2021-02-02 13:15:11 +0300

    Большая часть готова, единственное надо сделать красивую очистку, тк при повторном отрисовывании появляется что-то не то

--- lesson_004/05_snowfall.py
+++ lesson_004/05_snowfall.py
@@ -16,50 +16,55 @@ import random
 # sd.sleep()
 # sd.random_number()
 # sd.user_want_exit()
-def snowfall(n, coord, wind, speed):
-    lenght = 20
+def snowfall(n):
+    speed, coord, length = [], [], []
+    for i in range(n):
+        speed.append(random.randint(10, 50))
+        x = random.randint(50, 550)
+        y = random.randint(400, 550)
+        coord.append([x, y])
+        length.append(random.randint(10, 25))
     while True:
         for i in range(n):
+            wind = random.randint(-20, 20)
+            if speed[i] == 0:
+                speed.pop(i)
+                speed.insert(i, random.randint(5, 30))
+
+            if len(coord[i]) == 0:
+                length.pop(i)
+                length.insert(i, random.randint(10, 40))
+                coord.remove([])
+                x = random.randint(50, 550)
+                y = random.randint(500, 550)
+                coord.insert(i, [x, y])
+
+            coord[i][0] -= wind
+            coord[i][1] -= speed[i]
             x0, y0 = coord[i][0], coord[i][1]
             point = sd.get_point(x0, y0)
-            sd.snowflake(center=point, length=lenght)
-            if coord[i][1] <= lenght and speed != 0:
+            sd.snowflake(center=point, length=length[i])
+
+            if coord[i][1] <= 2*length[i] and speed != 0:
                 speed[i] = 0
-                wind[i] = 0
-                # coord.pop([i][0])
-                # coord.pop([i][1])
-            if coord[i][0] >= 580 and wind[i] > 0:
-                wind[i] = 0
-                # coord.pop([i][0])
-                # coord.pop([i][1])
-            if coord[i][0] <= 20 and wind[i] < 0:
-                wind[i] = 0
-                # coord.pop([i][0])
-                # coord.pop([i][1])
-            sd.sleep(.1)
+                coord[i].pop()
+                coord[i].pop()
+            # elif (coord[i][0] >= 560 and wind < 0) or (coord[i][0] <= 40 and wind > 0):
+            #     wind *= -1
+        sd.sleep(0.1)
         for i in range(n):
-            x0, y0 = coord[i][0], coord[i][1]
-            point = sd.get_point(x0, y0)
-            sd.snowflake(center=point, length=lenght, color=sd.background_color)
-        coord[i][0] -= wind[i]
-        coord[i][1] -= speed[i]
+            if len(coord[i]) != 0:
+                x0, y0 = coord[i][0], coord[i][1]
+                point = sd.get_point(x0, y0)
+                sd.snowflake(center=point, length=length[i], color=sd.background_color)
+
         if sd.user_want_exit():
             break
     sd.pause()
 
 
 def main():
-    speed = []
-    wind = []
-    snowflake_coord = []
-    # quan = int(input('Enter the num of snowflakes:'))
-    for i in range(3):
-        speed.append(random.randint(10, 50))
-        wind.append(random.randint(-20, 20))
-        x = random.randint(50, 550)
-        y = random.randint(400, 550)
-        snowflake_coord.append([x, y])
-    snowfall(3, snowflake_coord, wind, speed)
+    snowfall(10)
 
 
 main()

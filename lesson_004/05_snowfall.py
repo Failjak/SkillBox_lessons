971324ec0f8c05ef7358f49e939265135b9d69e9
Nikita <nikia.gushtyn200370@gmail.com>
2021-02-02 17:14:40 +0300

    Все сделано по красоте, поменяв местами и чутка поменяв код, все стало супер. i did task 05_snowfall.

--- lesson_004/05_snowfall.py
+++ lesson_004/05_snowfall.py
@@ -24,9 +24,28 @@ def snowfall(n):
         y = random.randint(400, 550)
         coord.append([x, y])
         length.append(random.randint(10, 25))
+
     while True:
         for i in range(n):
+            sd.start_drawing()
             wind = random.randint(-20, 20)
+
+            point = sd.get_point(coord[i][0], coord[i][1])
+            sd.snowflake(center=point, length=length[i], color=sd.background_color)
+            coord[i][0] -= wind
+            coord[i][1] -= speed[i]
+
+            x0, y0 = coord[i][0], coord[i][1]
+            point = sd.get_point(x0, y0)
+            sd.snowflake(center=point, length=length[i])
+
+            if coord[i][1] <= 2*length[i] and speed != 0:
+                speed[i] = 0
+                coord[i].pop()
+                coord[i].pop()
+
+            sd.finish_drawing()
+
             if speed[i] == 0:
                 speed.pop(i)
                 speed.insert(i, random.randint(5, 30))
@@ -39,25 +58,7 @@ def snowfall(n):
                 y = random.randint(500, 550)
                 coord.insert(i, [x, y])
 
-            coord[i][0] -= wind
-            coord[i][1] -= speed[i]
-            x0, y0 = coord[i][0], coord[i][1]
-            point = sd.get_point(x0, y0)
-            sd.snowflake(center=point, length=length[i])
-
-            if coord[i][1] <= 2*length[i] and speed != 0:
-                speed[i] = 0
-                coord[i].pop()
-                coord[i].pop()
-            # elif (coord[i][0] >= 560 and wind < 0) or (coord[i][0] <= 40 and wind > 0):
-            #     wind *= -1
         sd.sleep(0.1)
-        for i in range(n):
-            if len(coord[i]) != 0:
-                x0, y0 = coord[i][0], coord[i][1]
-                point = sd.get_point(x0, y0)
-                sd.snowflake(center=point, length=length[i], color=sd.background_color)
-
         if sd.user_want_exit():
             break
     sd.pause()

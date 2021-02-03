cd8b9dae75a40696007363e2e3eb7271d0400735
Nikita <nikia.gushtyn200370@gmail.com>
2021-02-03 19:19:07 +0300

    01_inhabitants is ready.

--- lesson_005/01_inhabitants.py
+++ lesson_005/01_inhabitants.py
@@ -1,8 +1,24 @@
 # -*- coding: utf-8 -*-
 
+from room_1 import folks as folks_1
+from room_2 import folks as folks_2
+
 # Вывести на консоль жителей комнат (модули room_1 и room_2)
 # Формат: В комнате room_1 живут: ...
 
-# TODO здесь ваш код
+
+def names(folks):
+    for name in folks:
+        if name == folks[-1]:
+            print(name, end='.')
+        else:
+            print(name, end=', ')
+
+
+print('In the room_1 living:', end=' ')
+names(folks_1)
+
+print('\nIn the room_2 living:', end=' ')
+names(folks_2)
 
 

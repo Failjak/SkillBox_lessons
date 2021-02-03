c7109719459c5c3bd8ad5eaf5175249e2cee55b9
Nikita <nikia.gushtyn200370@gmail.com>
2021-02-03 20:46:03 +0300

    02_district is ready.(But for me the solution is strange)

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 
+from district.central_street.house1 import room1 as cs_h1_r1, room2 as cs_h1_r2
+from district.central_street.house2 import room1 as cs_h2_r1, room2 as cs_h2_r2
+from district.soviet_street.house1 import room1 as ss_h1_r1, room2 as ss_h1_r2
+from district.soviet_street.house2 import room1 as ss_h2_r1, room2 as ss_h2_r2
+
+
 # Составить список всех живущих на районе и Вывести на консоль через запятую
 # Формат вывода: На районе живут ...
 # подсказка: для вывода элементов списка через запятую можно использовать функцию строки .join()
 # https://docs.python.org/3/library/stdtypes.html#str.join
 
-# TODO здесь ваш код
-
-
-
-
+print('На районе живут: ', ', '.join(cs_h1_r1.folks+cs_h1_r2.folks+cs_h2_r1.folks+cs_h2_r2.folks), ',', sep='')
+print(', '.join(ss_h1_r1.folks+ss_h1_r2.folks+ss_h2_r1.folks+ss_h2_r2.folks), '.', sep='')
